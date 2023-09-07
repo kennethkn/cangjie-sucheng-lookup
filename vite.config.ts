f6d5d49d2c1b61b01b8a145dd0c1f8e1d308cd89
Kenneth Kwan <123370597+kennethkn@users.noreply.github.com>
2023-09-07 19:44:29 +0800

    Add GA4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginRadar({
+    analytics: {
+      id: 'G-V7JPEKXC90',
+    },
+  })],
   base: '/chinese-to-cangjie-sucheng-converter/'
 })
